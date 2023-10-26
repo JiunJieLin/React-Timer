fbf5a452798651f740ce035fae73e8fbfb706277
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-10-26 22:52:12 +0800

    using ref to show entered name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,20 @@
-export default function Player() {
+import { useState, useRef } from "react";
+const Player = () => {
+  const playerName = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const handleClick = function () {
+    setEnteredName(playerName.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName || "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
-}
+};
+export default Player;
