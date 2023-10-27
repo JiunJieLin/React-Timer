7688a0889d75a621a223f8ed107b1b213b99abd8
JiunJieLin <sl10066116@gmail.com>
2023-10-27 20:21:58 +0800

    create ResultModal

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,30 +1,41 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
+
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
+
   const handleStart = () => {
     setTimerStarted(true);
-
-    setTimeout(() => {
+    //1秒之後去執行裡面的function
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
   };
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You Lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={handleStart}>
-          {timerStarted ? "Stop" : "Start"}Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? "active" : undefined}>
-        {timerStarted ? "Timer is running..." : "Timer inactive"}
-      </p>
-    </section>
+    <>
+      {timerExpired && <ResultModal result="lost" targetTime={targetTime} />}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"}Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Timer is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 };
 export default TimerChallenge;
